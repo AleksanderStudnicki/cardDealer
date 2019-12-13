478c712ddbb0bb138d24f3f62a22f07592472562
alek.studnicki@gmail.com <alek.studnicki@gmail.com>
2019-12-13 11:26:26 +0100

    replace one stream in dealer class with mapToObj, also code format

--- src/main/java/app/studnicki/carddealer/CardDealer.java
+++ src/main/java/app/studnicki/carddealer/CardDealer.java
@@ -5,17 +5,17 @@ import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.IntStream;
 
 class CardDealer {
-    static void dealCards(List<Player> players, List<Card> cards){
-        if(players == null){
+    static void dealCards(List<Player> players, List<Card> cards) {
+        if (players == null) {
             throw new IllegalArgumentException("Players cannot be null!");
         }
-        if(cards == null){
+        if (cards == null) {
             throw new IllegalArgumentException("Cards cannot be null!");
         }
-        if(players.size() < 1){
+        if (players.size() < 1) {
             throw new IllegalArgumentException("There must be at least 2 players");
         }
-        if(players.size() > cards.size()){
+        if (players.size() > cards.size()) {
             throw new IllegalArgumentException("There must be more cards than players!");
         }
 
@@ -23,9 +23,8 @@ class CardDealer {
         ThreadLocalRandom random = ThreadLocalRandom.current();
 
         IntStream.range(0, cards.size())
-                .forEach(i -> {
-                    Player player = players.get(i % pSize);
-
+                .mapToObj(i -> players.get(i % pSize))
+                .forEach(player -> {
                     int drawnIndex = random.nextInt(cards.size());
 
                     Card card = cards.get(drawnIndex);
